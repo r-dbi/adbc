8353801f89ed0e732f9ad900a3966b462bb1ef6e
Kirill Müller <krlmlr+r@mailbox.org>
2022-05-20 16:55:47 +0200

    Dummy implementation

--- src/code.cpp
+++ src/code.cpp
@@ -6,5 +6,17 @@ using namespace cpp11;
 [[cpp11::register]]
 void load_driver(const std::string connection) {
   AdbcDriver driver;
+  // TODO: version provided by client?
+  const size_t expected = ADBC_VERSION_0_0_1;
   size_t initialized = 0;
+
+  AdbcStatusCode success = AdbcLoadDriver(connection.c_str(), expected, &driver, &initialized);
+
+  if (success != ADBC_STATUS_OK) {
+    cpp11::stop("Could not load driver: %d.", success);
+  }
+
+  if (initialized < sizeof(driver)) {
+    cpp11::stop("Initialization incomplete: %d expected, %d provided.", expected, initialized);
+  }
 }
