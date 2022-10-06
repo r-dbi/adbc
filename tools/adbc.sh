253420a7ada072254155feecbbafbe7cfafe7505
Kirill Müller <krlmlr+r@mailbox.org>
2022-10-06 16:22:27 +0200

    Cleanup

--- tools/adbc.sh
+++ tools/adbc.sh
@@ -7,4 +7,3 @@ rm -rf src/adbc
 mkdir src/adbc
 cp -r ~/git/arrow-adbc/adbc.h src/adbc/
 cp -r ~/git/arrow-adbc/c/driver_manager src/adbc/driver_manager/
-# gsed -i 's#arrow/c#adbc#' src/adbc/*.cc src/adbc/*.h
